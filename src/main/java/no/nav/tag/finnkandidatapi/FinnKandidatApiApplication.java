646d56aa90bb416933efc1b0b200ee4d474b20d3
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2019-04-09 13:25:30 +0200

    Bootstrap spring-app

---
+++ src/main/java/no/nav/tag/finnkandidatapi/FinnKandidatApiApplication.java
@@ -0,0 +1,13 @@
+package no.nav.tag.finnkandidatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnKandidatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinnKandidatApiApplication.class, args);
+	}
+
+}

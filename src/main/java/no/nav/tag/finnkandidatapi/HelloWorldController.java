790f2bea5578f1af7064d646004f9741ef6869b2
André Mossige <mossige.a@gmail.com>
2019-04-10 12:53:01 +0200

    Lag hello world endepunkt

---
+++ src/main/java/no/nav/tag/finnkandidatapi/HelloWorldController.java
@@ -0,0 +1,13 @@
+package no.nav.tag.finnkandidatapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hallo fra backend!";
+    }
+}

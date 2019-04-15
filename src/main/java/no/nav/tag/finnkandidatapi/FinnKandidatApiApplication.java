9d5d3eed5f3839062659d7eee56b82b2589978da
André Mossige <mossige.a@gmail.com>
2019-04-15 09:49:10 +0200

    Legg til token-support

--- src/main/java/no/nav/tag/finnkandidatapi/FinnKandidatApiApplication.java
+++ src/main/java/no/nav/tag/finnkandidatapi/FinnKandidatApiApplication.java
@@ -1,9 +1,11 @@
 package no.nav.tag.finnkandidatapi;
 
+import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableOIDCTokenValidation
 public class FinnKandidatApiApplication {
 
 	public static void main(String[] args) {

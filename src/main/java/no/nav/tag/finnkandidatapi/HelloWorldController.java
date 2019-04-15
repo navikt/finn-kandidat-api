405e7f215ef42e7f639739881a4b1d2674633b34
André Mossige <mossige.a@gmail.com>
2019-04-15 12:08:37 +0200

    Legg til token-support (#5)
    * Legg til token-support
    
    * Fiks indentering i pom.xml
    
    * Fjern selvbetjening issuer

--- src/main/java/no/nav/tag/finnkandidatapi/HelloWorldController.java
+++ src/main/java/no/nav/tag/finnkandidatapi/HelloWorldController.java
@@ -1,5 +1,6 @@
 package no.nav.tag.finnkandidatapi;
 
+import no.nav.security.oidc.api.Protected;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/hello-world")
+    @Protected
     public String helloWorld() {
         return "Hallo fra backend!";
     }

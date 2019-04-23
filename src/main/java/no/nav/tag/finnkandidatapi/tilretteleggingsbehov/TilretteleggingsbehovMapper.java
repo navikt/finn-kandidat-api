4886c2999e2614b9c03251a96377b085a4b3ccc9
André Mossige <mossige.a@gmail.com>
2019-04-23 12:57:59 +0200

    Flytt db mappinglogikk til egen klasse

---
+++ src/main/java/no/nav/tag/finnkandidatapi/tilretteleggingsbehov/TilretteleggingsbehovMapper.java
@@ -0,0 +1,64 @@
+package no.nav.tag.finnkandidatapi.tilretteleggingsbehov;
+
+import lombok.SneakyThrows;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TilretteleggingsbehovMapper {
+
+    @SneakyThrows
+    public static Tilretteleggingsbehov map(ResultSet rs, int rowNum) {
+        return Tilretteleggingsbehov.builder()
+                .id(rs.getInt("id"))
+                .opprettet(rs.getTimestamp("opprettet").toLocalDateTime())
+                .opprettetAvIdent(rs.getString("opprettet_av_ident"))
+                .brukerFnr(rs.getString("bruker_fnr"))
+                .arbeidstid(Arbeidstid.valueOf(rs.getString("arbeidstid")))
+                .fysisk(tilFysisk(rs.getString("fysisk")))
+                .arbeidsmiljo(tilArbeidsmiljo(rs.getString("arbeidsmiljo")))
+                .grunnleggende(tilGrunnleggende(rs.getString("grunnleggende")))
+                .build();
+    }
+
+    private static ArrayList<Fysisk> tilFysisk(String string) {
+        return Stream.of(string.split(","))
+                .map(Fysisk::valueOf)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    static String fraFysisk(ArrayList<Fysisk> list) {
+        String[] stringlist = list.stream()
+                .map(Enum::name)
+                .toArray(String[]::new);
+        return String.join(",", stringlist);
+    }
+
+    private static ArrayList<Arbeidsmiljo> tilArbeidsmiljo(String string) {
+        return Stream.of(string.split(","))
+                .map(Arbeidsmiljo::valueOf)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    static String fraArbeidsmiljo(ArrayList<Arbeidsmiljo> list) {
+        String[] stringlist = list.stream()
+                .map(Enum::name)
+                .toArray(String[]::new);
+        return String.join(",", stringlist);
+    }
+
+    private static ArrayList<Grunnleggende> tilGrunnleggende(String string) {
+        return Stream.of(string.split(","))
+                .map(Grunnleggende::valueOf)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    static String fraGrunnleggende(ArrayList<Grunnleggende> list) {
+        String[] stringlist = list.stream()
+                .map(Enum::name)
+                .toArray(String[]::new);
+        return String.join(",", stringlist);
+    }
+}
--- src/main/java/no/nav/tag/finnkandidatapi/tilretteleggingsbehov/TilretteleggingsbehovRepository.java
+++ src/main/java/no/nav/tag/finnkandidatapi/tilretteleggingsbehov/TilretteleggingsbehovRepository.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import static no.nav.tag.finnkandidatapi.tilretteleggingsbehov.TilretteleggingsbehovMapper.*;
 
 @Repository
 public class TilretteleggingsbehovRepository {
@@ -33,55 +31,8 @@ public class TilretteleggingsbehovRepository {
     Tilretteleggingsbehov hentTilretteleggingsbehov(Integer id) {
         return jdbcTemplate.queryForObject(
                 "SELECT * FROM tilretteleggingsbehov WHERE id = ?", new Object[] { id },
-                (rs, rowNum) -> Tilretteleggingsbehov.builder()
-                        .id(rs.getInt("id"))
-                        .opprettet(rs.getTimestamp("opprettet").toLocalDateTime())
-                        .opprettetAvIdent(rs.getString("opprettet_av_ident"))
-                        .brukerFnr(rs.getString("bruker_fnr"))
-                        .arbeidstid(Arbeidstid.valueOf(rs.getString("arbeidstid")))
-                        .fysisk(tilFysisk(rs.getString("fysisk")))
-                        .arbeidsmiljo(tilArbeidsmiljo(rs.getString("arbeidsmiljo")))
-                        .grunnleggende(tilGrunnleggende(rs.getString("grunnleggende")))
-                        .build()
+                TilretteleggingsbehovMapper::map
         );
     }
 
-    private ArrayList<Fysisk> tilFysisk(String string) {
-        return Stream.of(string.split(","))
-                .map(Fysisk::valueOf)
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
-
-    private String fraFysisk(ArrayList<Fysisk> list) {
-        String[] stringlist = list.stream()
-                .map(Enum::name)
-                .toArray(String[]::new);
-        return String.join(",", stringlist);
-    }
-
-    private ArrayList<Arbeidsmiljo> tilArbeidsmiljo(String string) {
-        return Stream.of(string.split(","))
-                .map(Arbeidsmiljo::valueOf)
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
-
-    private String fraArbeidsmiljo(ArrayList<Arbeidsmiljo> list) {
-        String[] stringlist = list.stream()
-                .map(Enum::name)
-                .toArray(String[]::new);
-        return String.join(",", stringlist);
-    }
-
-    private ArrayList<Grunnleggende> tilGrunnleggende(String string) {
-        return Stream.of(string.split(","))
-                .map(Grunnleggende::valueOf)
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
-
-    private String fraGrunnleggende(ArrayList<Grunnleggende> list) {
-        String[] stringlist = list.stream()
-                .map(Enum::name)
-                .toArray(String[]::new);
-        return String.join(",", stringlist);
-    }
 }

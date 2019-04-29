e3fd12b2eb4e1e3cacf81e54b51ade6543a63e9f
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-04-29 15:25:30 +0200

    TAG-363 Legg til endepunkt for å hente innlogget bruker

--- src/main/java/no/nav/tag/finnkandidatapi/tilgangskontroll/TilgangskontrollController.java
+++ src/main/java/no/nav/tag/finnkandidatapi/tilgangskontroll/TilgangskontrollController.java
@@ -1,25 +1,32 @@
 package no.nav.tag.finnkandidatapi.tilgangskontroll;
 
+import lombok.RequiredArgsConstructor;
 import no.nav.security.oidc.api.Protected;
-import no.nav.security.oidc.api.Unprotected;
+import no.nav.tag.finnkandidatapi.kandidat.TokenUtils;
+import no.nav.tag.finnkandidatapi.kandidat.Veileder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Protected
+@RequiredArgsConstructor
 @RestController
 public class TilgangskontrollController {
 
     private final TilgangskontrollService tilgangskontrollService;
-
-    public TilgangskontrollController(TilgangskontrollService tilgangskontrollService) {
-        this.tilgangskontrollService = tilgangskontrollService;
-    }
+    private final TokenUtils tokenUtils;
 
     @GetMapping("/harTilgang/{fnr}")
-    @Protected
     public boolean harTilgang(
             @PathVariable("fnr") String fnr
     ) {
         return tilgangskontrollService.harTilgang(fnr);
     }
+
+    @GetMapping("/innlogget-bruker")
+    public ResponseEntity<Veileder> innloggetBruker() {
+        return ResponseEntity.ok(tokenUtils.hentInnloggetVeileder());
+    }
+
 }

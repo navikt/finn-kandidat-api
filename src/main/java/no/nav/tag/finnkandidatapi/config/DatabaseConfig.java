950b7c13647d63ea7af3957426c55ecd0748c1d0
André Mossige <mossige.a@gmail.com>
2019-04-30 15:29:21 +0200

    Fiks bug i profiloppsett

--- src/main/java/no/nav/tag/finnkandidatapi/config/DatabaseConfig.java
+++ src/main/java/no/nav/tag/finnkandidatapi/config/DatabaseConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Profile;
 import javax.sql.DataSource;
 
 @Configuration
-@Profile({"dev, prod"})
+@Profile({"dev", "prod"})
 public class DatabaseConfig {
 
     @Value("${database.url}")

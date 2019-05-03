a6466915f293ab5e8a7cf187da004fc51ff26f02
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2019-05-03 13:46:55 +0200

    Sett opp enkel metrikkregistrering for opprettet kandidat

--- src/main/java/no/nav/tag/finnkandidatapi/kandidat/KandidatController.java
+++ src/main/java/no/nav/tag/finnkandidatapi/kandidat/KandidatController.java
@@ -3,18 +3,26 @@ package no.nav.tag.finnkandidatapi.kandidat;
 
 import lombok.RequiredArgsConstructor;
 import no.nav.security.oidc.api.Protected;
+import no.nav.tag.finnkandidatapi.Kjetil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.AbstractAggregateRoot;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Protected
 @RestController
 @RequestMapping("/kandidater")
 @RequiredArgsConstructor
-public class KandidatController {
-
+public class KandidatController extends AbstractAggregateRoot<KandidatController> {
+    private final ApplicationEventPublisher applicationEventPublisher;
     private final KandidatService kandidatService;
     private final TokenUtils tokenUtils;
 
@@ -34,6 +42,8 @@ public class KandidatController {
     public ResponseEntity<Kandidat> lagreKandidat(@RequestBody Kandidat kandidat) {
         Veileder veileder = tokenUtils.hentInnloggetVeileder();
         Kandidat lagretKandidat = kandidatService.lagreKandidat(kandidat, veileder).orElseThrow(FinnKandidatException::new);
+        applicationEventPublisher.publishEvent(lagretKandidat);
+
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(lagretKandidat);

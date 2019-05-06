950c21e1f7f18aecafe09cf73f585112d4086008
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-05-06 13:54:30 +0200

    fjern masse greier

--- src/main/java/no/nav/tag/finnkandidatapi/tilgangskontroll/TilgangskontrollController.java
+++ src/main/java/no/nav/tag/finnkandidatapi/tilgangskontroll/TilgangskontrollController.java
@@ -1,10 +1,7 @@
 package no.nav.tag.finnkandidatapi.tilgangskontroll;
 
-import no.nav.security.oidc.api.Protected;
 import no.nav.security.oidc.api.Unprotected;
 import no.nav.tag.finnkandidatapi.kandidat.Veileder;
-import no.nav.tag.finnkandidatapi.tilgangskontroll.abac.AbacClient;
-import no.nav.tag.finnkandidatapi.tilgangskontroll.abac.response.XacmlResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +13,10 @@ public class TilgangskontrollController {
 
     private final TilgangskontrollService tilgangskontrollService;
     private final TokenUtils tokenUtils;
-    private final AbacClient abacClient;
 
-    public TilgangskontrollController(TilgangskontrollService tilgangskontrollService, TokenUtils tokenUtils, AbacClient abacClient) {
+    public TilgangskontrollController(TilgangskontrollService tilgangskontrollService, TokenUtils tokenUtils) {
         this.tilgangskontrollService = tilgangskontrollService;
         this.tokenUtils = tokenUtils;
-        this.abacClient = abacClient;
     }
 
     @GetMapping("/harTilgang/{fnr}")
@@ -30,12 +25,6 @@ public class TilgangskontrollController {
     ) {
         // TODO TAG-363: Bare en testcontroller. Skal fjernes
         return tilgangskontrollService.harSkrivetilgangTilKandidat(fnr);
-    }
-
-    @GetMapping("/abac")
-    public XacmlResponse kallAbac() {
-        // TODO TAG-363: Bare en testcontroller. Skal fjernes
-        return abacClient.ping();
     }
 
     @GetMapping("/innlogget-bruker")
--- src/main/java/no/nav/tag/finnkandidatapi/tilgangskontroll/TilgangskontrollService.java
+++ src/main/java/no/nav/tag/finnkandidatapi/tilgangskontroll/TilgangskontrollService.java
@@ -2,43 +2,25 @@ package no.nav.tag.finnkandidatapi.tilgangskontroll;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.tag.finnkandidatapi.kandidat.Kandidat;
 import no.nav.tag.finnkandidatapi.kandidat.Veileder;
-import no.nav.tag.finnkandidatapi.tilgangskontroll.veilarbabac.VeilarbabacClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.function.Supplier;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TilgangskontrollService {
-    private final VeilarbabacClient veilarbabacClient;
     private final TokenUtils tokenUtils;
 
     public boolean harSkrivetilgangTilKandidat(String fnr) {
-        if (true) return true;
-        return veilarbabacClient.harSkrivetilgangTilKandidat(fnr);
+        return true;
+    }
+
+    public void sjekkSkrivetilgangTilKandidat(String fnr) {
+        return;
     }
 
     public void sjekkLesetilgangTilKandidat(String fnr) {
         // TODO Implementeres i TAG-500
-    }
-
-    public void sjekkSkrivetilgangTilKandidat(String fnr) {
-        if (true) return;
-        if (!veilarbabacClient.harSkrivetilgangTilKandidat(fnr)) {
-            log.error(
-                    "Veileder {} har ikke skrivetilgang til kandidat {}",
-                    tokenUtils.hentInnloggetVeileder().getNavIdent(),
-                    fnr
-            );
-            throw new TilgangskontrollException();
-        }
     }
 
     public Veileder hentInnloggetVeileder() {

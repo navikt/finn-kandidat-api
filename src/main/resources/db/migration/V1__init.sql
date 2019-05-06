b8ab8d28e631e45bc28d8d7ed3176f1d8e4d18eb
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2019-05-06 13:15:57 +0200

    Bruk best practice for string-lagring i PostgreSQL-database

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -3,8 +3,8 @@ CREATE TABLE kandidat (
     fnr VARCHAR(11),
     registrert_av VARCHAR(7),
     registreringstidspunkt TIMESTAMP(6) DEFAULT current_timestamp,
-    arbeidstid_behov VARCHAR(255),
-    fysiske_behov VARCHAR(1000),
-    arbeidsmiljø_behov VARCHAR(1000),
-    grunnleggende_behov VARCHAR(1000)
+    arbeidstid_behov TEXT,
+    fysiske_behov TEXT,
+    arbeidsmiljø_behov TEXT,
+    grunnleggende_behov TEXT
 );

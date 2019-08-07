6443273e0a23e760c86ad6dbfb790c007665411c
André Mossige <mossige.a@gmail.com>
2019-08-07 15:21:46 +0200

    Erstatt bindestrek med punktum i gauge metrikk

--- src/main/java/no/nav/tag/finnkandidatapi/metrikker/AntallKandidaterGauge.java
+++ src/main/java/no/nav/tag/finnkandidatapi/metrikker/AntallKandidaterGauge.java
@@ -9,6 +9,6 @@ public class AntallKandidaterGauge {
 
     public AntallKandidaterGauge(MeterRegistry meterRegistry, KandidatRepository repository) {
         // TODO: Fjern 10
-        meterRegistry.gauge("finn-kandidat.kandidater.antall", repository, repositoriet -> repositoriet.hentKandidater().size() + 10);
+        meterRegistry.gauge("finn.kandidat.kandidater.antall", repository, repositoriet -> repositoriet.hentKandidater().size() + 10);
     }
 }

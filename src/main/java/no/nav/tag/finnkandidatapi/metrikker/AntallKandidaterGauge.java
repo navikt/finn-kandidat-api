e220701865f71b03afd69c70a346bf9e88fefb5e
André Mossige <mossige.a@gmail.com>
2019-08-07 14:59:13 +0200

    Legg til gauge for antall kandidater

---
+++ src/main/java/no/nav/tag/finnkandidatapi/metrikker/AntallKandidaterGauge.java
@@ -0,0 +1,13 @@
+package no.nav.tag.finnkandidatapi.metrikker;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import no.nav.tag.finnkandidatapi.kandidat.KandidatRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AntallKandidaterGauge {
+
+    public AntallKandidaterGauge(MeterRegistry meterRegistry, KandidatRepository repository) {
+        meterRegistry.gauge("finn-kandidat.kandidater.antall", repository, repositoriet -> repositoriet.hentKandidater().size());
+    }
+}

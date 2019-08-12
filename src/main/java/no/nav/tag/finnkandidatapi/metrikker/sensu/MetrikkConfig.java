07268435c18bb4ed7ce2f289df4b8f4ef9c42ff2
André Mossige <mossige.a@gmail.com>
2019-08-12 14:26:34 +0200

    Test å sette miljø selv i metrics bibliotek

--- src/main/java/no/nav/tag/finnkandidatapi/metrikker/sensu/MetrikkConfig.java
+++ src/main/java/no/nav/tag/finnkandidatapi/metrikker/sensu/MetrikkConfig.java
@@ -2,6 +2,7 @@ package no.nav.tag.finnkandidatapi.metrikker.sensu;
 
 import no.nav.metrics.MetricsClient;
 import no.nav.metrics.MetricsConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -9,7 +10,10 @@ import org.springframework.context.annotation.Profile;
 @Profile({ "dev", "prod" })
 public class MetrikkConfig {
 
+    @Value("${NAIS_CLUSTER_NAME}")
+    private static String miljø;
+
     static {
-        MetricsClient.enableMetrics(MetricsConfig.resolveNaisConfig());
+        MetricsClient.enableMetrics(MetricsConfig.resolveNaisConfig().withEnvironment(miljø));
     }
 }

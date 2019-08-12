810ec5e0bbab750eedd1c618ddb4ba53089b65af
LarsAndreasNav <36694755+LarsAndreasNav@users.noreply.github.com>
2019-08-12 14:00:52 +0200

    Legg til swagger (#36)
    * Legg til swagger
    
    * Ignorer hele org.springframework pakken fra login

--- src/main/java/no/nav/tag/finnkandidatapi/FinnKandidatApiApplication.java
+++ src/main/java/no/nav/tag/finnkandidatapi/FinnKandidatApiApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableOIDCTokenValidation
+@EnableOIDCTokenValidation(ignore = {
+		"springfox.documentation.swagger.web.ApiResourceController",
+		"org.springframework"
+})
 public class FinnKandidatApiApplication {
 
 	public static void main(String[] args) {

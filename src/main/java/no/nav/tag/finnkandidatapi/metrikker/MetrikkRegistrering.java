de7c789c129eccdcda03615c4b545e118c897e91
André Mossige <mossige.a@gmail.com>
2019-08-19 15:30:58 +0200

    Fjern ubrukt logging

--- src/main/java/no/nav/tag/finnkandidatapi/metrikker/MetrikkRegistrering.java
+++ src/main/java/no/nav/tag/finnkandidatapi/metrikker/MetrikkRegistrering.java
@@ -12,20 +12,16 @@ public class MetrikkRegistrering {
 
     @EventListener
     public void kandidatOpprettet(KandidatOpprettet event) {
-        log.info("event=kandidat.opprettet, id={}", event.getKandidat().getId());
         MetricsFactory.createEvent("finn-kandidat.kandidat.opprettet").report();
     }
 
     @EventListener
     public void kandidatEndret(KandidatEndret event) {
-        log.info("event=kandidat.endret, id={}", event.getKandidat().getId());
         MetricsFactory.createEvent("finn-kandidat.kandidat.endret").report();
     }
 
     @EventListener
     public void kandidatSlettet(KandidatSlettet event) {
-        log.info("event=kandidat.slettet, id={}", event.getId());
-
         if (event.getSlettetAv().equals(Brukertype.VEILEDER)) {
             MetricsFactory.createEvent("finn-kandidat.kandidat.slettet")
                     .addTagToReport("slettetAv", Brukertype.VEILEDER.name())

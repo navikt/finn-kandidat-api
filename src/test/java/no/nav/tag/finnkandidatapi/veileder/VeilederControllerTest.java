207d4c012f5a296cd17f5adecb8f3c57d2c78cab
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2019-09-10 09:06:19 +0200

    Lag endepunkt for henting av innlogget veileders id (#46)
    * Lag endepunkt for henting av innlogget veileders id
    
    * Flytt endepunkt til /veileder/me

---
+++ src/test/java/no/nav/tag/finnkandidatapi/veileder/VeilederControllerTest.java
@@ -0,0 +1,42 @@
+package no.nav.tag.finnkandidatapi.veileder;
+
+import no.nav.tag.finnkandidatapi.kandidat.Veileder;
+import no.nav.tag.finnkandidatapi.tilgangskontroll.TilgangskontrollService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static no.nav.tag.finnkandidatapi.TestData.enVeileder;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VeilederControllerTest {
+    private VeilederController controller;
+
+    @Mock
+    private TilgangskontrollService tilgangskontroll;
+
+    @Before
+    public void setUp() {
+        controller = new VeilederController(tilgangskontroll);
+    }
+
+    @Test
+    public void hentInnloggetVeileder__skal_returnere_innlogget_veileder_sin_navident() {
+        Veileder veileder = enVeileder();
+        værInnloggetSom(veileder);
+
+        ResponseEntity respons = controller.hentInnloggetVeileder();
+        assertThat(respons.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(respons.getBody()).isEqualTo(veileder.getNavIdent());
+    }
+
+    private void værInnloggetSom(Veileder veileder) {
+        when(tilgangskontroll.hentInnloggetVeileder()).thenReturn(veileder);
+    }
+}

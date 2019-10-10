9b2c1c9c5a5b918e59b88d329e9cb7d1c1452852
André Mossige <andre.mossige@nav.no>
2019-10-10 10:03:52 +0200

    Legg til featuretoggling av produsering av kafkameldinger til PAM (#53)
    * Legg til featuretoggling av produsering av kafkameldinger til PAM
    
    * Bruk ByClusterStrategy i stedet for snart-deprecated ByEnvironmentStrategy

---
+++ src/main/java/no/nav/tag/finnkandidatapi/unleash/UnleashConfiguration.java
@@ -0,0 +1,43 @@
+package no.nav.tag.finnkandidatapi.unleash;
+
+import no.finn.unleash.DefaultUnleash;
+import no.finn.unleash.FakeUnleash;
+import no.finn.unleash.Unleash;
+import no.finn.unleash.util.UnleashConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class UnleashConfiguration {
+
+    private static final String APP_NAME = "finn-kandidat-api";
+
+    public static final String HAR_TILRETTELEGGINGSBEHOV_PRODUCER_FEATURE =
+            "finnkandidat.hartilretteleggingsbehov.producer";
+
+    @Profile({"dev", "prod"})
+    @Bean
+    public Unleash unleash(
+            ByClusterStrategy byClusterStrategy,
+            @Value("${unleash.url}") String unleashUrl,
+            @Value("${spring.profiles.active}") String profile
+    ) {
+        UnleashConfig config = UnleashConfig.builder()
+                .appName(APP_NAME)
+                .instanceId(APP_NAME + "-" + profile)
+                .unleashAPI(unleashUrl)
+                .build();
+
+        return new DefaultUnleash(config, byClusterStrategy);
+    }
+
+    @Profile({"local", "mock"})
+    @Bean
+    public Unleash unleashMock() {
+        FakeUnleash fakeUnleash = new FakeUnleash();
+        fakeUnleash.enable(HAR_TILRETTELEGGINGSBEHOV_PRODUCER_FEATURE);
+        return fakeUnleash;
+    }
+}

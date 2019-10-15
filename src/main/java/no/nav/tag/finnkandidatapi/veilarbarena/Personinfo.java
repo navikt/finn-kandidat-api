aa72f0f13b90b2e9bc31af6b41833b31513f8ec6
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2019-10-15 12:33:31 +0200

    Mock veilarbarena

--- src/main/java/no/nav/tag/finnkandidatapi/veilarbarena/Personinfo.java
+++ src/main/java/no/nav/tag/finnkandidatapi/veilarbarena/Personinfo.java
@@ -1,10 +1,14 @@
 package no.nav.tag.finnkandidatapi.veilarbarena;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Personinfo {
     public String nav_kontor;
 }

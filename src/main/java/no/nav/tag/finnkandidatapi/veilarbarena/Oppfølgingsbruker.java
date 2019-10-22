e7c33a3f51f871247897c58b815fd7b5a16a08b5
André Mossige <mossige.a@gmail.com>
2019-10-22 12:26:42 +0200

    Ignorer uventa felter i Oppfølgingsbruker

--- src/main/java/no/nav/tag/finnkandidatapi/veilarbarena/Oppfølgingsbruker.java
+++ src/main/java/no/nav/tag/finnkandidatapi/veilarbarena/Oppfølgingsbruker.java
@@ -1,5 +1,6 @@
 package no.nav.tag.finnkandidatapi.veilarbarena;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Oppfølgingsbruker {
 
     @JsonProperty("fodselsnr")

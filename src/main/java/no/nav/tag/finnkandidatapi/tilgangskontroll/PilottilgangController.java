e54f686f2317595ccd4bbccd6ca0c724ce43d75b
André Mossige <andre.mossige@nav.no>
2020-01-23 13:21:05 +0100

    Pilotkontortilgang endepunkt (#77)
    * Legg til feature toggle controller
    
    * Deploy til dev
    
    * Returner JSON i pilottilgangcontroller
    
    * Rename pilottilgang controller metode
    
    * Fjern branch fra circleci config

---
+++ src/main/java/no/nav/tag/finnkandidatapi/tilgangskontroll/PilottilgangController.java
@@ -0,0 +1,25 @@
+package no.nav.tag.finnkandidatapi.tilgangskontroll;
+
+import lombok.RequiredArgsConstructor;
+import no.nav.security.oidc.api.Protected;
+import no.nav.tag.finnkandidatapi.unleash.FeatureToggleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static no.nav.tag.finnkandidatapi.tilgangskontroll.TilgangskontrollService.FINN_KANDIDAT_PILOTTILGANG_KONTOR;
+
+@RestController
+@Protected
+@RequiredArgsConstructor
+public class PilottilgangController {
+
+    private final FeatureToggleService featureToggleService;
+
+    @GetMapping("/pilottilgang")
+    public ResponseEntity<PilottilgangRespons> harPilottilgang() {
+        boolean harTilgang = featureToggleService.isEnabled(FINN_KANDIDAT_PILOTTILGANG_KONTOR);
+        PilottilgangRespons respons = new PilottilgangRespons(harTilgang);
+        return ResponseEntity.ok(respons);
+    }
+}

4aa6db83f38afe5d881bf7e93c23f1c6836ced13
André Mossige <andre.mossige@nav.no>
2020-01-23 09:24:13 +0100

    Valider OpenAM-tokens (#75)
    * Legg til OpenAM issuer
    
    * Fiks bygging på CircleCI
    
    * Støtt OpenAM-tokens i 'hentInnloggetVeileder'
    
    * Hent openam oidctoken hvis azuretoken ikke fins
    
    * Deploy til dev
    
    * Legg til veilarblogin-q0 audience
    
    * Legg til ingresser som FO ligger på
    
    * Fiks mergefeil
    
    * Forenkle hentOidcToken metode
    
    * Forenkle metodenavn
    
    Co-authored-by: Kjetil Svalestuen <kjesvale@users.noreply.github.com>

--- src/main/java/no/nav/tag/finnkandidatapi/tilgangskontroll/TokenUtils.java
+++ src/main/java/no/nav/tag/finnkandidatapi/tilgangskontroll/TokenUtils.java
@@ -4,7 +4,6 @@ import com.nimbusds.jwt.JWTClaimsSet;
 import no.nav.security.oidc.context.OIDCClaims;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
 import no.nav.tag.finnkandidatapi.kandidat.Veileder;
-import org.apache.zookeeper.Op;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +13,7 @@ import java.util.Optional;
 public class TokenUtils {
 
     final static String ISSUER_ISSO = "isso";
+    final static String ISSUER_OPENAM = "openam";
 
     private final OIDCRequestContextHolder contextHolder;
 
@@ -23,24 +23,37 @@ public class TokenUtils {
     }
 
     public String hentOidcToken() {
-        return contextHolder.getOIDCValidationContext().getToken(ISSUER_ISSO).getIdToken();
+        String issuer = erInnloggetMedAzureAD() ? ISSUER_ISSO : ISSUER_OPENAM;
+        return contextHolder.getOIDCValidationContext().getToken(issuer).getIdToken();
     }
 
     public Veileder hentInnloggetVeileder() {
-        if (erInnloggetNavAnsattMedAzureADToken()) {
+        if (erInnloggetMedAzureAD()) {
             String navIdent = hentClaim(ISSUER_ISSO, "NAVident")
                     .orElseThrow(() -> new TilgangskontrollException("Innlogget bruker er ikke veileder."));
+            return new Veileder(navIdent);
+        } else if (erInnloggetMedOpenAM()) {
+            String navIdent = contextHolder.getOIDCValidationContext().getClaims(ISSUER_OPENAM).getSubject();
             return new Veileder(navIdent);
         } else {
             throw new TilgangskontrollException("Bruker er ikke innlogget.");
         }
     }
 
-    private boolean erInnloggetNavAnsattMedAzureADToken() {
+    private boolean erInnloggetMedAzureAD() {
         Optional<String> navIdent = hentClaimSet(ISSUER_ISSO)
                 .map(jwtClaimsSet -> (String) jwtClaimsSet.getClaims().get("NAVident"))
                 .filter(this::erNAVIdent);
         return navIdent.isPresent();
+    }
+
+    private boolean erInnloggetMedOpenAM() {
+        OIDCClaims claims = contextHolder.getOIDCValidationContext().getClaims(ISSUER_OPENAM);
+        if (claims == null) {
+            return false;
+        }
+
+        return erNAVIdent(claims.getSubject());
     }
 
     private Optional<String> hentClaim(String issuer, String claim) {

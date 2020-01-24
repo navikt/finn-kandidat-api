9f0d3d8d4481c0ba5d0df8dfc2245e0b1b72da12
André Mossige <mossige.a@gmail.com>
2020-01-24 13:42:16 +0100

    Hent kandidat med fnr

--- src/main/java/no/nav/tag/finnkandidatapi/kandidat/KandidatController.java
+++ src/main/java/no/nav/tag/finnkandidatapi/kandidat/KandidatController.java
@@ -25,6 +25,13 @@ public class KandidatController {
     private final KandidatService kandidatService;
     private final TilgangskontrollService tilgangskontroll;
 
+    @GetMapping("/fnr/{fnr}")
+    public ResponseEntity<Kandidat> hentKandidatMedFnr(@PathVariable("fnr") String fnr) {
+        loggBrukAvEndepunkt("hentKandidat");
+        String aktørId = kandidatService.hentAktørId(fnr);
+        return hentKandidat(aktørId);
+    }
+
     @GetMapping("/{aktørId}")
     public ResponseEntity<Kandidat> hentKandidat(@PathVariable("aktørId") String aktørId) {
         loggBrukAvEndepunkt("hentKandidat");

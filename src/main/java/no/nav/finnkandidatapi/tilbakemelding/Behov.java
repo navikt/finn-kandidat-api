0a63b4f828177dc199adba27bbc3227eed5c551d
André Mossige <mossige.a@gmail.com>
2020-03-02 09:15:06 +0100

    Fiks feil ved uthenting av gamle tilbakemeldinger

--- src/main/java/no/nav/finnkandidatapi/tilbakemelding/Behov.java
+++ src/main/java/no/nav/finnkandidatapi/tilbakemelding/Behov.java
@@ -1,8 +1,12 @@
 package no.nav.finnkandidatapi.tilbakemelding;
 
+// Bruker ikke behovkategori i tilbakemeldinger lengre, så må fjerne denne
+@Deprecated
 public enum Behov {
     ARBEIDSHVERDAGEN,
     ARBEIDSTID,
     FYSISK,
-    UTFORDRINGER_MED_NORSK
+    UTFORDRINGER_MED_NORSK,
+    ARBEIDSMILJØ,
+    GRUNNLEGGENDE
 }

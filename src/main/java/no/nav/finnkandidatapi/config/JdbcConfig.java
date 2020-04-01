3e033730f8068f34b447881e0c862659c91f9711
Hågen Hasle <haagenhasle@gmail.com>
2020-04-01 15:23:42 +0200

    Fikset en bug fordi jeg gjenbruke SimpleJdbcInsert. Har nå gjort den om til Prototype scope

--- src/main/java/no/nav/finnkandidatapi/config/JdbcConfig.java
+++ src/main/java/no/nav/finnkandidatapi/config/JdbcConfig.java
@@ -1,7 +1,9 @@
 package no.nav.finnkandidatapi.config;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
@@ -9,6 +11,7 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 public class JdbcConfig {
 
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public SimpleJdbcInsert simpleJdbcInsert(JdbcTemplate jdbcTemplate) {
         return new SimpleJdbcInsert(jdbcTemplate);
     }

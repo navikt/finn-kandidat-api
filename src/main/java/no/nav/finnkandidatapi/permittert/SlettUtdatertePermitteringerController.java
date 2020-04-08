2f8b6b5e65dcfafe4c2f09468dd9c3ac33812526
André Mossige <mossige.a@gmail.com>
2020-04-08 09:59:19 +0200

    Legg på logging

--- src/main/java/no/nav/finnkandidatapi/permittert/SlettUtdatertePermitteringerController.java
+++ src/main/java/no/nav/finnkandidatapi/permittert/SlettUtdatertePermitteringerController.java
@@ -1,5 +1,6 @@
 package no.nav.finnkandidatapi.permittert;
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.finnkandidatapi.aktørregister.AktørRegisterClient;
 import no.nav.finnkandidatapi.veilarboppfolging.VeilarbOppfolgingClient;
 import no.nav.security.token.support.core.api.Protected;
@@ -7,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 @Protected
 @Controller
 public class SlettUtdatertePermitteringerController {
@@ -29,12 +33,19 @@ public class SlettUtdatertePermitteringerController {
     @DeleteMapping("/permitteringer")
     public void slettAlleUtdatertePermitteringer() {
         List<PermittertArbeidssoker> arbeidssøkere = permittertArbeidssokerRepository.hentAllePermitterteArbeidssokere();
+        log.info("Fant {} permitterte arbeidssøkere", arbeidssøkere.size());
+
+        AtomicInteger antallSletta = new AtomicInteger();
         arbeidssøkere.forEach(permittertArbeidssoker -> {
             String fnr = aktørRegisterClient.tilFnr(permittertArbeidssoker.getAktørId());
             boolean erUnderOppfølging = veilarbOppfolgingClient.hentOppfølgingsstatus(fnr).isUnderOppfolging();
             if (!erUnderOppfølging) {
-                permittertArbeidssokerRepository.slettPermittertArbeidssoker(permittertArbeidssoker.getAktørId());
+                Optional<Integer> id = permittertArbeidssokerRepository.slettPermittertArbeidssoker(permittertArbeidssoker.getAktørId());
+                log.info("Slettet permittert arbeidssøker. Ny rad id: {}", id);
+                antallSletta.getAndIncrement();
             }
         });
+
+        log.info("Sletta {} permitterte arbeidssøkere ikke under oppfølging", antallSletta.get());
     }
 }

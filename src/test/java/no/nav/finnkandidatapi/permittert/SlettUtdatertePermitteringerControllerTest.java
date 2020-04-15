09b1571c1bb6bd35fa7a8dfe6dfd0add0eba0150
André Mossige <mossige.a@gmail.com>
2020-04-15 08:48:57 +0200

    Prøv å fikse feil i tester

--- src/test/java/no/nav/finnkandidatapi/permittert/SlettUtdatertePermitteringerControllerTest.java
+++ src/test/java/no/nav/finnkandidatapi/permittert/SlettUtdatertePermitteringerControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles({"local", "mock"})
+@DirtiesContext
 class SlettUtdatertePermitteringerControllerTest {
 
     @Autowired

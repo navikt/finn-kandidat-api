997469b56047fcdb60a31f16f16bf8db31d5c9ca
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-04-16 09:24:57 +0200

    Lag mocket POST-endepunkt

--- src/main/java/no/nav/finnkandidatapi/midlertidigUtilgjengelig/MidlertidigUtilgjengeligController.java
+++ src/main/java/no/nav/finnkandidatapi/midlertidigUtilgjengelig/MidlertidigUtilgjengeligController.java
@@ -3,6 +3,7 @@ package no.nav.finnkandidatapi.midlertidigUtilgjengelig;
 import no.nav.finnkandidatapi.kandidat.Veileder;
 import no.nav.finnkandidatapi.tilgangskontroll.TilgangskontrollService;
 import no.nav.security.oidc.api.Protected;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,10 +22,20 @@ public class MidlertidigUtilgjengeligController {
     }
 
     @GetMapping("/{aktørId}")
-    public ResponseEntity<MidlertidigUtilgjengelig> hentMidlertidigUtilgjengelig(@PathVariable("aktørId") String aktørId) {
+    public ResponseEntity<MidlertidigUtilgjengelig> getMidlertidigUtilgjengelig(@PathVariable("aktørId") String aktørId) {
         Veileder innloggetVeileder = tilgangskontroll.hentInnloggetVeileder();
 
         MidlertidigUtilgjengelig status = service.hentMidlertidigUtilgjengelig(aktørId, innloggetVeileder);
         return ResponseEntity.ok(status);
+    }
+
+    @PostMapping
+    public ResponseEntity postMidlertidigUtilgjengelig(@RequestBody MidlertidigUtilgjengeligDTO midlertidigUtilgjengelig) {
+        Veileder innloggetVeileder = tilgangskontroll.hentInnloggetVeileder();
+
+        MidlertidigUtilgjengelig lagretMidlertidigUtilgjengelig = service.lagreMidlertidigUtilgjengelig(midlertidigUtilgjengelig, innloggetVeileder);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(lagretMidlertidigUtilgjengelig);
     }
 }
--- src/main/java/no/nav/finnkandidatapi/midlertidigUtilgjengelig/MidlertidigUtilgjengeligService.java
+++ src/main/java/no/nav/finnkandidatapi/midlertidigUtilgjengelig/MidlertidigUtilgjengeligService.java
@@ -9,11 +9,24 @@ import java.time.LocalDateTime;
 public class MidlertidigUtilgjengeligService {
 
     public MidlertidigUtilgjengelig hentMidlertidigUtilgjengelig(String aktørId, Veileder innloggetVeileder) {
+        // TODO: Snakk med repository
         return MidlertidigUtilgjengelig.builder()
                 .id(1)
                 .aktørId("0000000000")
                 .fraDato(LocalDateTime.now())
                 .tilDato(LocalDateTime.now().plusDays(15))
+                .registreringstidspunkt(LocalDateTime.now())
+                .registrertAv(innloggetVeileder.getNavIdent())
+                .build();
+    }
+
+    public MidlertidigUtilgjengelig lagreMidlertidigUtilgjengelig(MidlertidigUtilgjengeligDTO midlertidigUtilgjengelig, Veileder innloggetVeileder) {
+        // TODO: Snakk med repository
+        return MidlertidigUtilgjengelig.builder()
+                .id(1)
+                .aktørId(midlertidigUtilgjengelig.getAktørId())
+                .fraDato(midlertidigUtilgjengelig.getFraDato())
+                .tilDato(midlertidigUtilgjengelig.getTilDato())
                 .registreringstidspunkt(LocalDateTime.now())
                 .registrertAv(innloggetVeileder.getNavIdent())
                 .build();

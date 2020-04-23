183ecf116eae85237f5f294d2e1d71ec4f4903f5
Joar Aurdal <joar.aurdal@nav.no>
2020-04-23 15:16:02 +0200

    Fjern enablescheduling import

--- src/main/java/no/nav/finnkandidatapi/FinnKandidatApiApplication.java
+++ src/main/java/no/nav/finnkandidatapi/FinnKandidatApiApplication.java
@@ -3,17 +3,16 @@ package no.nav.finnkandidatapi;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJwtTokenValidation(ignore = {
-		"springfox.documentation.swagger.web.ApiResourceController",
-		"org.springframework"
+        "springfox.documentation.swagger.web.ApiResourceController",
+        "org.springframework"
 })
 public class FinnKandidatApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinnKandidatApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FinnKandidatApiApplication.class, args);
+    }
 
 }

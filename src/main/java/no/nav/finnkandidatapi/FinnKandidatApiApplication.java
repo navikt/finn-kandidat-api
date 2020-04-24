456dd2fe37c8c95b8cc8fcb5ac38cb7032f11669
Joar Aurdal <joar.aurdal@nav.no>
2020-04-24 15:49:45 +0200

    Legg til lock for midlertidig utilgjengelig scheduler

--- src/main/java/no/nav/finnkandidatapi/FinnKandidatApiApplication.java
+++ src/main/java/no/nav/finnkandidatapi/FinnKandidatApiApplication.java
@@ -1,10 +1,8 @@
 package no.nav.finnkandidatapi;
 
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJwtTokenValidation(ignore = {

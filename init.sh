cedc54006be857e673b252022f771f04d40eec5d
Are Husby <are.husby@nav.no>
2020-06-05 15:02:29 +0200

    Loggmelding feil i tekst

--- init.sh
+++ init.sh
@@ -9,5 +9,5 @@ if test -f "/secret/serviceuser/password"; then
   export SERVICEUSER_PASSWORD=$(cat /secret/serviceuser/password)
   echo "Eksporterer variabel SERVICEUSER_PASSWORD"
 else
-  echo "Eksporterer IKKE variabel SERVICEUSER_PASSWORD fordi filen /secret/serviceuser/username ikke finnes"
+  echo "Eksporterer IKKE variabel SERVICEUSER_PASSWORD fordi filen /secret/serviceuser/password ikke finnes"
 fi

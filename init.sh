fdf791c10f0fc21ef862086fc67cd1f28fcf9704
Are Husby <are.husby@nav.no>
2020-06-05 14:55:07 +0200

    Revert "Stavefeil"
    This reverts commit 68f2dbc1

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 if test -f "/secrets/serviceuser/username"; then
-  export SERVICEUSER_USERNAME=$(cat /secrets/serviceuser/username)
+  export SERVICEUSER_USERNAME=$(cat /secret/serviceuser/username)
   echo "Eksporterer variabel SERVICEUSER_USERNAME"
 else
   echo "Eksporterer IKKE variabel SERVICEUSER_USERNAME fordi filen /secrets/serviceuser/username ikke finnes"
 fi
 
 if test -f "/secrets/serviceuser/password"; then
-  export SERVICEUSER_PASSWORD=$(cat /secrets/serviceuser/password)
+  export SERVICEUSER_PASSWORD=$(cat /secret/serviceuser/password)
   echo "Eksporterer variabel SERVICEUSER_PASSWORD"
 else
   echo "Eksporterer IKKE variabel SERVICEUSER_PASSWORD fordi filen /secrets/serviceuser/username ikke finnes"

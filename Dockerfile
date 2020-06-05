c65bb9404634c4ddcbc669fbf9a87c7a2dc13f53
Are Husby <are.husby@nav.no>
2020-06-05 07:37:10 +0200

    Tilgjengeliggjør serviceuser credentials som miljøvariabler

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM navikt/java:13
+COPY init.sh /init-scripts/init.sh
 COPY /target/finn-kandidat-api-0.0.1-SNAPSHOT.jar app.jar
--- src/main/java/no/nav/finnkandidatapi/tilgangskontroll/TilgangskontrollConfig.java
+++ src/main/java/no/nav/finnkandidatapi/tilgangskontroll/TilgangskontrollConfig.java
@@ -2,13 +2,25 @@ package no.nav.finnkandidatapi.tilgangskontroll;
 
 import no.nav.common.abac.Pep;
 import no.nav.common.abac.VeilarbPep;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TilgangskontrollConfig {
+
+    @Value("${abac.url}")
+    private String abacUrl;
+
+    @Value("${SERVICEUSER_USERNAME}")
+    private String srvUsername;
+
+    @Value(("${SERVICEUSER_PASSWORD}"))
+    private String srvPassword;
+
+
     @Bean
     public Pep veilarbPep() {
-        return new VeilarbPep("https://wasapp-q0.adeo.no/asm-pdp/authorize", "srvusername", "srvpassword"); // TODO Are
+        return new VeilarbPep(abacUrl, srvUsername, srvPassword);
     }
 }

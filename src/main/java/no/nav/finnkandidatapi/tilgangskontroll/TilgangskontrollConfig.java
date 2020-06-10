4ea5a2298e7be14821a2f466d5ea91bbbd0faa74
Are Husby <are.husby@nav.no>
2020-06-10 09:42:08 +0200

    Gir opp audit logging i denne omgang, revert tilbake til kun fungerende tilgangskontroll

--- src/main/java/no/nav/finnkandidatapi/tilgangskontroll/TilgangskontrollConfig.java
+++ src/main/java/no/nav/finnkandidatapi/tilgangskontroll/TilgangskontrollConfig.java
@@ -2,7 +2,6 @@ package no.nav.finnkandidatapi.tilgangskontroll;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.abac.Pep;
-import no.nav.common.abac.SpringAuditRequestInfoSupplier;
 import no.nav.common.abac.VeilarbPep;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +25,6 @@ public class TilgangskontrollConfig {
     public Pep veilarbPep() {
         String srvPwLength = srvPassword != null ? String.valueOf(srvPassword.length()) : "null";
         log.info("ABAC autentisering konfig: URL=[" + abacUrl + "], srvUsername=[" + srvUsername + "], srvPassword.length=" + srvPwLength);
-        return new VeilarbPep(abacUrl, srvUsername, srvPassword, new SpringAuditRequestInfoSupplier());
+        return new VeilarbPep(abacUrl, srvUsername, srvPassword);
     }
 }

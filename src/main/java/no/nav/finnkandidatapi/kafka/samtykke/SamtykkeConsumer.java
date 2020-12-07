29ba3ac25fb1b4ce35858a201e33bc2010aaa8c0
Joar Aurdal <joar.aurdal@nav.no>
2020-12-07 16:10:16 +0100

    Fiks slf4j

--- src/main/java/no/nav/finnkandidatapi/kafka/samtykke/SamtykkeConsumer.java
+++ src/main/java/no/nav/finnkandidatapi/kafka/samtykke/SamtykkeConsumer.java
@@ -1,9 +1,11 @@
 package no.nav.finnkandidatapi.kafka.samtykke;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class SamtykkeConsumer {
 

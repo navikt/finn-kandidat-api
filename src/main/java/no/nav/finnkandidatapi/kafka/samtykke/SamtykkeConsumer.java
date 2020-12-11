f71b1ec6e71d2f8c336ad82c5bbf68a4646aa529
Joar Aurdal <joar.aurdal@gmail.com>
2020-12-11 15:21:25 +0100

    Samtykke kafka (#149)
    * Sett opp consumer for samtykke
    
    * Fiks slf4j
    
    * Deploy til dev
    
    * Linjeskift for å få ut i dev
    
    * Legg til test-prefiks i groupId
    
    * Legg til testmetode som genererer topic record for samtykke
    
    * Endre log og groupid
    
    * bump
    
    * La til kafka factory
    
    * bump
    
    * Legg til consumer aware for samtykke
    
    * Legg til consumer aware for samtykke
    
    * uten containerlisternerfactory
    
    * Legg til offset earliest
    
    * Ta bort testconf fra prod
    
    * Offset config opprydning
    
    * Deaktiver samtykke lytter midlertidig
    
    Co-authored-by: Mads Lee Giil <mads.lee.giil@nav.no>

---
+++ src/main/java/no/nav/finnkandidatapi/kafka/samtykke/SamtykkeConsumer.java
@@ -0,0 +1,27 @@
+package no.nav.finnkandidatapi.kafka.samtykke;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.ConsumerSeekAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class SamtykkeConsumer implements ConsumerSeekAware {
+
+    /*@KafkaListener(
+            topics = "aapen-pam-samtykke-endret-v1",
+            groupId = "finn-kandidat-samtykke",
+            clientIdPrefix = "samtykke",
+            containerFactory = "kafkaListenerContainerFactory"
+    )*/
+    public void konsumerMelding(ConsumerRecord<String, String> melding) {
+        String json = melding.value();
+        log.info("jsonsamtykke: " + json);
+    }
+
+}

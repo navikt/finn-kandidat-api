300620347c57a72a3d703cc98c996014221507dc
André Mossige <andre.mossige@nav.no>
2021-04-14 10:26:23 +0200

    Slett ubrukt endepunkt som ble brukt til ryddejobb (#175)

--- src/main/java/no/nav/finnkandidatapi/veilarboppfolging/VeilarbOppfolgingClient.java
+++ src/main/java/no/nav/finnkandidatapi/veilarboppfolging/VeilarbOppfolgingClient.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
@@ -19,33 +18,15 @@ public class VeilarbOppfolgingClient {
     private final RestTemplate restTemplate;
     private final TokenUtils tokenUtils;
     private final String url;
-    private final STSClient stsClient;
 
     public VeilarbOppfolgingClient(
             RestTemplate restTemplate,
             TokenUtils tokenUtils,
-            @Value("${veilarboppfolging.url}") String url,
-            STSClient stsClient
+            @Value("${veilarboppfolging.url}") String url
     ) {
         this.restTemplate = restTemplate;
         this.tokenUtils = tokenUtils;
         this.url = url;
-        this.stsClient = stsClient;
-    }
-
-    public Oppfølgingsstatus hentOppfølgingsstatus(String fnr) {
-        String uri = UriComponentsBuilder
-                .fromHttpUrl(url)
-                .path("/underoppfolging")
-                .queryParam("fnr", fnr)
-                .toUriString();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(stsClient.hentSTSToken().getAccessToken());
-        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-
-        ResponseEntity<Oppfølgingsstatus> respons = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, Oppfølgingsstatus.class);
-        return respons.getBody();
     }
 
     public Oppfølgingsstatus hentOppfølgingsstatus() {

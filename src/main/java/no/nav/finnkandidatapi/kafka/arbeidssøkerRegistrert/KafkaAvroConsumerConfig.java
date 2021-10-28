04942ce29b3da6ff853d26c23f950bce29376edc
Are Husby <are.husby@nav.no>
2021-10-28 10:54:31 +0200

    Konsumer fra ny topic (#191)
    * Konsumerer arbeidssoker registrert fra ny topic i aiven
    
    * bug
    
    * deploy også denne featurebranchen til dev
    
    * Slett ubrukt konfig
    
    etter at topic-navn har blitt flyttet til koden istedenfor å være i konfigfil
    
    * Logg topic-navn ved suksess og ved feil
    
    * Kjør ev. integrasjonstester på Github
    
    fordi vi så i en eller annen app at det var mange tester som nesten aldri ble kjørt fordu utviklerne stolte på at tester ble kjørt av Github og pushet uten å kjøre integrasjonstester lokalt (uten f.eks. mvn clean install)
    
    Co-authored-by: Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>

--- src/main/java/no/nav/finnkandidatapi/kafka/arbeidssøkerRegistrert/KafkaAvroConsumerConfig.java
+++ src/main/java/no/nav/finnkandidatapi/kafka/arbeidssøkerRegistrert/KafkaAvroConsumerConfig.java
@@ -1,10 +1,16 @@
 package no.nav.finnkandidatapi.kafka.arbeidssøkerRegistrert;
 
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClientConfig;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,9 +32,30 @@ import java.util.Map;
 @Profile("!local")
 public class KafkaAvroConsumerConfig {
 
+    @Value("${KAFKA_BROKERS:http://dummyurl.com:0000}")
+    private String brokersUrl;
+
+    @Value("${KAFKA_KEYSTORE_PATH:}")
+    private String keystorePath;
+
+    @Value("${KAFKA_TRUSTSTORE_PATH:}")
+    private String truststorePath;
+
+    @Value("${KAFKA_CREDSTORE_PASSWORD:}")
+    private String credstorePassword;
+
+    @Value("${KAFKA_SCHEMA_REGISTRY:}")
+    private String schemaRegistryUrl;
+
+    @Value("${KAFKA_SCHEMA_REGISTRY_USER:}")
+    private String schemaRegistryUser;
+
+    @Value("${KAFKA_SCHEMA_REGISTRY_PASSWORD:}")
+    private String schemaRegistryPassword;
+
     @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> avroKafkaListenerContainerFactory(
-            @Qualifier("avroConsumerFactory") ConsumerFactory<String, String> consumerFactory
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> avroAivenKafkaListenerContainerFactory(
+            @Qualifier("avroAivenConsumerFactory") ConsumerFactory<String, String> consumerFactory
     ) {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
@@ -41,8 +68,24 @@ public class KafkaAvroConsumerConfig {
     }
 
     @Bean
-    public ConsumerFactory<String, String> avroConsumerFactory(KafkaProperties properties) {
+    public ConsumerFactory<String, String> avroAivenConsumerFactory(KafkaProperties properties) {
         Map<String, Object> consumerProperties = properties.buildConsumerProperties();
+
+        consumerProperties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        String basicAuth = schemaRegistryUser + ":" + schemaRegistryPassword;
+        consumerProperties.put(SchemaRegistryClientConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO"); // magic constant 🤯
+        consumerProperties.put(SchemaRegistryClientConfig.USER_INFO_CONFIG, basicAuth);
+
+        consumerProperties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        consumerProperties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, brokersUrl);
+
+        if (StringUtils.isNotEmpty(keystorePath)) {
+            consumerProperties.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, credstorePassword);
+            consumerProperties.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, credstorePassword);
+            consumerProperties.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, truststorePath);
+            consumerProperties.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, keystorePath);
+        }
+
         consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         consumerProperties.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, KafkaAvroDeserializer.class);

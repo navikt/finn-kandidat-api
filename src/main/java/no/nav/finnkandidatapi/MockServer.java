7c65f7c65a31dd0c0e529ceeb81cb2ad1a6c00c5
Joar Aurdal <joar.aurdal@nav.no>
2022-10-11 12:38:20 +0200

    Ta bort synlighetskall

--- src/main/java/no/nav/finnkandidatapi/MockServer.java
+++ src/main/java/no/nav/finnkandidatapi/MockServer.java
@@ -25,14 +25,12 @@ public class MockServer implements DisposableBean {
 
     @Autowired
     MockServer(@Value("${mock.port}") Integer port,
-               @Value("${abac.url}") String abacUrl,
-               @Value("${arbeidssoker.url}") String pamCvApiUrl) {
+               @Value("${abac.url}") String abacUrl) {
         log.info("Starter mockserver");
 
         this.server =  new WireMockServer(port);
 
         mockAbac(abacUrl);
-        mockPamCvApi(pamCvApiUrl);
         mockPdl();
 
         server.start();
@@ -49,171 +47,6 @@ public class MockServer implements DisposableBean {
                                 "    \"Decision\": \"Permit\"\n" +
                                 "  }\n" +
                                 "}")
-                )
-        );
-    }
-
-    private void mockPamCvApi(String pamCvApiUrl) {
-        String path = getPath(pamCvApiUrl + "/rest/v2/arbeidssoker/1");
-        server.stubFor(
-                get(urlPathEqualTo(path)).willReturn(WireMock.aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withStatus(HttpStatus.OK.value())
-                        .withBody(
-                                "{\n" +
-                                        "    \"sistEndret\": \"2021-06-15T14:09:22.434703+02:00\",\n" +
-                                        "    \"synligForArbeidsgiver\": false,\n" +
-                                        "    \"sistEndretAvNav\": false,\n" +
-                                        "    \"sammendrag\": \"Jeg har gjort litt av hvert og har mange fine egenskaper\",\n" +
-                                        "    \"arbeidserfaring\": [\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Rektor\",\n" +
-                                        "            \"arbeidsgiver\": \"Drammen barneskole\",\n" +
-                                        "            \"sted\": null,\n" +
-                                        "            \"beskrivelse\": \"Litt forskjellig rektorrelatert\",\n" +
-                                        "            \"fraDato\": \"2004-08\",\n" +
-                                        "            \"tilDato\": \"2008-08\"\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"utdanning\": [\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Bachelor i historie\",\n" +
-                                        "            \"studiested\": \"Universitetet i Tromsø\",\n" +
-                                        "            \"beskrivelse\": \"Historie\",\n" +
-                                        "            \"fraDato\": \"1980-08\",\n" +
-                                        "            \"tilDato\": \"1984-06\"\n" +
-                                        "        },\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Danselinja\",\n" +
-                                        "            \"studiested\": \"Drammen forlkehøgskole\",\n" +
-                                        "            \"beskrivelse\": \"Dans\",\n" +
-                                        "            \"fraDato\": \"1979-07\",\n" +
-                                        "            \"tilDato\": \"1980-05\"\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"fagdokumentasjoner\": [\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Svennebrev baker\",\n" +
-                                        "            \"type\": \"SVENNEBREV_FAGBREV\"\n" +
-                                        "        },\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Fagbrev aluminiumskonstruktør\",\n" +
-                                        "            \"type\": \"SVENNEBREV_FAGBREV\"\n" +
-                                        "        },\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Mesterbrev anleggsgartner\",\n" +
-                                        "            \"type\": \"MESTERBREV\"\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"godkjenninger\": [\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Skipskokk\",\n" +
-                                        "            \"utsteder\": null,\n" +
-                                        "            \"gjennomfortDato\": \"2021-06\",\n" +
-                                        "            \"utloperDato\": \"2025-10\"\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"annenErfaring\": [\n" +
-                                        "        {\n" +
-                                        "            \"rolle\": \"Fotballtrener\",\n" +
-                                        "            \"beskrivelse\": \"10 års erfaring som fotballtrener for barn\",\n" +
-                                        "            \"fraDato\": null,\n" +
-                                        "            \"tilDato\": null\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"forerkort\": [\n" +
-                                        "        {\n" +
-                                        "            \"klasse\": \"A - Motorsykkel\",\n" +
-                                        "            \"fraDato\": null,\n" +
-                                        "            \"utloperDato\": null\n" +
-                                        "        },\n" +
-                                        "        {\n" +
-                                        "            \"klasse\": \"B - Personbil\",\n" +
-                                        "            \"fraDato\": null,\n" +
-                                        "            \"utloperDato\": null\n" +
-                                        "        },\n" +
-                                        "        {\n" +
-                                        "            \"klasse\": \"D - Buss\",\n" +
-                                        "            \"fraDato\": \"2010-02-15\",\n" +
-                                        "            \"utloperDato\": \"2025-10-15\"\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"kurs\": [\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Franskkurs\",\n" +
-                                        "            \"arrangor\": \"Fransk i Norge AS\",\n" +
-                                        "            \"fraDato\": null,\n" +
-                                        "            \"varighet\": {\n" +
-                                        "                \"varighet\": null,\n" +
-                                        "                \"tidsenhet\": null\n" +
-                                        "            }\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"sertifikater\": [\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Renholdsattest\",\n" +
-                                        "            \"utsteder\": null,\n" +
-                                        "            \"gjennomfortDato\": \"2011-01\",\n" +
-                                        "            \"utloperDato\": null\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"andreGodkjenninger\": [\n" +
-                                        "        {\n" +
-                                        "            \"tittel\": \"Renholdsattest\",\n" +
-                                        "            \"utsteder\": null,\n" +
-                                        "            \"gjennomfortDato\": \"2011-01\",\n" +
-                                        "            \"utloperDato\": null\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"sprak\": [\n" +
-                                        "        {\n" +
-                                        "            \"sprak\": \"Engelsk\",\n" +
-                                        "            \"muntligNiva\": \"IKKE_OPPGITT\",\n" +
-                                        "            \"skriftligNiva\": \"IKKE_OPPGITT\"\n" +
-                                        "        },\n" +
-                                        "        {\n" +
-                                        "            \"sprak\": \"Norsk tegnspråk\",\n" +
-                                        "            \"muntligNiva\": \"IKKE_OPPGITT\",\n" +
-                                        "            \"skriftligNiva\": \"IKKE_OPPGITT\"\n" +
-                                        "        }\n" +
-                                        "    ],\n" +
-                                        "    \"jobbprofil\": {\n" +
-                                        "        \"sistEndret\": \"2021-06-15T14:09:22.301432+02:00\",\n" +
-                                        "        \"onsketYrke\": [\n" +
-                                        "            {\n" +
-                                        "                \"tittel\": \"Sanger\"\n" +
-                                        "            }\n" +
-                                        "        ],\n" +
-                                        "        \"onsketArbeidssted\": [\n" +
-                                        "            {\n" +
-                                        "                \"stedsnavn\": \"Valle\"\n" +
-                                        "            }\n" +
-                                        "        ],\n" +
-                                        "        \"onsketAnsettelsesform\": [\n" +
-                                        "            {\n" +
-                                        "                \"tittel\": \"FAST\"\n" +
-                                        "            }\n" +
-                                        "        ],\n" +
-                                        "        \"onsketArbeidstidsordning\": [\n" +
-                                        "            {\n" +
-                                        "                \"tittel\": \"DAGTID\"\n" +
-                                        "            }\n" +
-                                        "        ],\n" +
-                                        "        \"heltidDeltid\": {\n" +
-                                        "            \"heltid\": true,\n" +
-                                        "            \"deltid\": false\n" +
-                                        "        },\n" +
-                                        "        \"kompetanse\": [\n" +
-                                        "            {\n" +
-                                        "                \"tittel\": \"Elektronikk\"\n" +
-                                        "            },\n" +
-                                        "            {\n" +
-                                        "                \"tittel\": \"Sangteknikk\"\n" +
-                                        "            }\n" +
-                                        "        ]\n" +
-                                        "    }\n" +
-                                        "}"
-                        )
                 )
         );
     }

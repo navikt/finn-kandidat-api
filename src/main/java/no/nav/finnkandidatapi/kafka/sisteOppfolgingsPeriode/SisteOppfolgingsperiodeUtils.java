4ea220811dd8338e56d017ecfe19da9cff419750
vildestefferud <qwartz.ms@hotmail.com>
2022-12-14 22:33:26 +0100

    Fiks skrivefeil

--- src/main/java/no/nav/finnkandidatapi/kafka/sisteOppfolgingsPeriode/SisteOppfolgingsperiodeUtils.java
+++ src/main/java/no/nav/finnkandidatapi/kafka/sisteOppfolgingsPeriode/SisteOppfolgingsperiodeUtils.java
@@ -2,6 +2,7 @@ package no.nav.finnkandidatapi.kafka.sisteOppfolgingsPeriode;
 
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import no.nav.pto_schema.kafka.json.topic.SisteOppfolgingsperiodeV1;
 
 import java.io.IOException;
@@ -10,16 +11,17 @@ public class SisteOppfolgingsperiodeUtils {
     public static SisteOppfolgingsperiodeV1 deserialiserMelding(String melding) {
         try {
             ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
             mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
-            SisteOppfolgingsperiodeV1 sisteOppfolgingsperiod = mapper.readValue(melding, SisteOppfolgingsperiodeV1.class);
+            SisteOppfolgingsperiodeV1 sisteOppfolgingsperiode = mapper.readValue(melding, SisteOppfolgingsperiodeV1.class);
 
-            if (sisteOppfolgingsperiod.getAktorId().isEmpty() || sisteOppfolgingsperiod.getStartDato() == null) {
-                throw new RuntimeException("Ugyldig data for siste oppfolging periode på bruker: " + sisteOppfolgingsperiod.getAktorId());
+            if (sisteOppfolgingsperiode.getAktorId().isEmpty() || sisteOppfolgingsperiode.getStartDato() == null) {
+                throw new RuntimeException("Ugyldig data for siste oppfolging periode på bruker: " + sisteOppfolgingsperiode.getAktorId());
             }
-            if (sisteOppfolgingsperiod.getSluttDato() != null && sisteOppfolgingsperiod.getStartDato().isAfter(sisteOppfolgingsperiod.getSluttDato())) {
-                throw new RuntimeException("Ugyldig start/slutt dato for siste oppfolging periode på bruker: " + sisteOppfolgingsperiod.getAktorId());
+            if (sisteOppfolgingsperiode.getSluttDato() != null && sisteOppfolgingsperiode.getStartDato().isAfter(sisteOppfolgingsperiode.getSluttDato())) {
+                throw new RuntimeException("Ugyldig start/slutt dato for siste oppfolging periode på bruker: " + sisteOppfolgingsperiode.getAktorId());
             }
-            return sisteOppfolgingsperiod;
+            return sisteOppfolgingsperiode;
 
         } catch (IOException e) {
             throw new RuntimeException("Kunne ikke deserialisere SisteOppfolgingsPeriode", e);

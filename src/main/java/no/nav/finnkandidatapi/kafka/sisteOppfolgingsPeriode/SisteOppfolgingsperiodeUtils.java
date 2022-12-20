3e532499cc79ac07a2f503f8d7345639a8f80751
Are Husby <are.husby@nav.no>
2022-12-20 12:44:19 +0100

    Fiks rød test (ZonedDateTime)

--- src/main/java/no/nav/finnkandidatapi/kafka/sisteOppfolgingsPeriode/SisteOppfolgingsperiodeUtils.java
+++ src/main/java/no/nav/finnkandidatapi/kafka/sisteOppfolgingsPeriode/SisteOppfolgingsperiodeUtils.java
@@ -8,13 +8,12 @@ import no.nav.pto_schema.kafka.json.topic.SisteOppfolgingsperiodeV1;
 import java.io.IOException;
 
 public class SisteOppfolgingsperiodeUtils {
+
+    public static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule()).configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+
     public static SisteOppfolgingsperiodeV1 deserialiserMelding(String melding) {
         try {
-            ObjectMapper mapper = new ObjectMapper()
-                    .registerModule(new JavaTimeModule())
-                    .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
-
-            SisteOppfolgingsperiodeV1 sisteOppfolgingsperiode = mapper.readValue(melding, SisteOppfolgingsperiodeV1.class);
+            SisteOppfolgingsperiodeV1 sisteOppfolgingsperiode = objectMapper.readValue(melding, SisteOppfolgingsperiodeV1.class);
 
             if (sisteOppfolgingsperiode.getAktorId().isEmpty() || sisteOppfolgingsperiode.getStartDato() == null) {
                 throw new RuntimeException("Ugyldig data for siste oppfolgingsperiode på bruker: " + sisteOppfolgingsperiode.getAktorId());
--- src/test/java/no/nav/finnkandidatapi/kandidat/SisteOppfolgingsperiodeUtilsTest.java
+++ src/test/java/no/nav/finnkandidatapi/kandidat/SisteOppfolgingsperiodeUtilsTest.java
@@ -1,52 +1,37 @@
 package no.nav.finnkandidatapi.kandidat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.SneakyThrows;
 import no.nav.finnkandidatapi.kafka.sisteOppfolgingsPeriode.SisteOppfolgingsperiodeUtils;
 import no.nav.pto_schema.kafka.json.topic.SisteOppfolgingsperiodeV1;
 import org.junit.Test;
 
-import java.time.ZonedDateTime;
-import java.util.UUID;
-
+import static java.time.ZonedDateTime.now;
+import static java.util.UUID.randomUUID;
 import static no.nav.finnkandidatapi.TestData.enAktørId;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SisteOppfolgingsperiodeUtilsTest {
-    private ObjectMapper objectMapper = new ObjectMapper()
-            .registerModule(new JavaTimeModule());
+    private final ObjectMapper objectMapper = SisteOppfolgingsperiodeUtils.objectMapper;
 
     @SneakyThrows
     @Test(expected = RuntimeException.class)
     public void deserialiserMelding__skal_kaste_runtime_exception_hvis_aktørId_er_null() {
-        SisteOppfolgingsperiodeV1 meldingUtenAktørId = new SisteOppfolgingsperiodeV1(
-                UUID.randomUUID(),
-                null,
-                ZonedDateTime.now().minusYears(2),
-                ZonedDateTime.now());
+        SisteOppfolgingsperiodeV1 meldingUtenAktørId = new SisteOppfolgingsperiodeV1(randomUUID(), null, now().minusYears(2), now());
         SisteOppfolgingsperiodeUtils.deserialiserMelding(objectMapper.writeValueAsString(meldingUtenAktørId));
     }
 
     @SneakyThrows
     @Test(expected = RuntimeException.class)
     public void deserialiserMelding__skal_kaste_runtime_exception_hvis_startDato_er_null() {
-        SisteOppfolgingsperiodeV1 meldingUtenSluttDato = new SisteOppfolgingsperiodeV1(
-                UUID.randomUUID(),
-                enAktørId(),
-                null,
-                ZonedDateTime.now());
+        SisteOppfolgingsperiodeV1 meldingUtenSluttDato = new SisteOppfolgingsperiodeV1(randomUUID(), enAktørId(), null, now());
         SisteOppfolgingsperiodeUtils.deserialiserMelding(objectMapper.writeValueAsString(meldingUtenSluttDato));
     }
 
     @SneakyThrows
     @Test(expected = RuntimeException.class)
     public void deserialiserMelding__skal_kaste_runtime_exception_hvis_startDato_er_etter_sluttDato() {
-        SisteOppfolgingsperiodeV1 meldingUtenSluttDato = new SisteOppfolgingsperiodeV1(
-                UUID.randomUUID(),
-                enAktørId(),
-                ZonedDateTime.now(),
-                ZonedDateTime.now().minusYears(2));
+        SisteOppfolgingsperiodeV1 meldingUtenSluttDato = new SisteOppfolgingsperiodeV1(randomUUID(), enAktørId(), now(), now().minusYears(2));
         SisteOppfolgingsperiodeUtils.deserialiserMelding(objectMapper.writeValueAsString(meldingUtenSluttDato));
     }
 
@@ -58,12 +43,11 @@ public class SisteOppfolgingsperiodeUtilsTest {
     @SneakyThrows
     @Test
     public void deserialiserMelding__skal_deserialisere_gyldig_melding() {
-        SisteOppfolgingsperiodeV1 gyldigMelding = new SisteOppfolgingsperiodeV1(
-                UUID.randomUUID(),
-                enAktørId(),
-                ZonedDateTime.now().minusYears(2),
-                ZonedDateTime.now());
-        SisteOppfolgingsperiodeV1 deserialisertMelding = SisteOppfolgingsperiodeUtils.deserialiserMelding(objectMapper.writeValueAsString(gyldigMelding));
-        assertThat(deserialisertMelding).isEqualTo(gyldigMelding);
+        SisteOppfolgingsperiodeV1 expected = new SisteOppfolgingsperiodeV1(randomUUID(), enAktørId(), now().minusYears(2), now());
+        SisteOppfolgingsperiodeV1 actual = SisteOppfolgingsperiodeUtils.deserialiserMelding(objectMapper.writeValueAsString(expected));
+        assertThat(actual.getAktorId()).isEqualTo(expected.getAktorId());
+        assertThat(actual.getUuid()).isEqualTo(expected.getUuid());
+        assertThat(actual.getStartDato()).isEqualTo(expected.getStartDato());
+        assertThat(actual.getSluttDato()).isEqualTo(expected.getSluttDato());
     }
 }
